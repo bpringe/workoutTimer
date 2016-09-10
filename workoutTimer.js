a745ef586ec5af7ebc0673154e0db2463f6144c0
Brandon Ringe <brandon.ringe@gmail.com>
2016-09-09 23:32:32 -0500

    Initial commit

---
+++ workoutTimer.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    var secondsElement = $('#seconds');
+    var minutesElement = $('#minutes');
+    var intervalSecondsElement = $('#intervalSeconds');
+    var intervalMinutesElement = $('#intervalMinutes');
+    var totalSeconds = 0;
+    var seconds = 0;
+    var minutes = 0;
+    var secondsDisplay;
+    var minutesDisplay;
+    var intervalSeconds = 0;
+
+    $('#startTimerButton').click(startTimer);
+
+    function startTimer() {
+        intervalSeconds = Number(intervalMinutesElement.val() * 60)
+            + Number(intervalSecondsElement.val());
+
+        if (intervalSeconds === 0) {
+            return;
+        }
+
+        setInterval(count, 1000);
+    }
+
+    function count() {
+        totalSeconds += 1;
+
+        if (totalSeconds % intervalSeconds === 0) {
+            console.log('Interval hit');
+        }
+
+        seconds = totalSeconds % 60;
+
+        if (seconds === 0) {
+            minutes += 1;
+        }
+
+        displayClock();
+    }
+
+    function displayClock() {
+        secondsDisplay = (seconds < 10) ? '0' + seconds : seconds;
+        minutesDisplay = (minutes < 10) ? '0' + minutes : minutes;
+        secondsElement.text(secondsDisplay);
+        minutesElement.text(minutesDisplay);
+    }
+});
